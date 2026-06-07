e87a4ad22a770bc149f66dff8c96014735275c93
Joseph-Brendan <199966678+Ironali@users.noreply.github.com>
2026-06-07 11:31:32 +0100

    feat: add site favicon and manifest configuration

--- src/components/shared/SEO.tsx
+++ src/components/shared/SEO.tsx
@@ -54,8 +54,11 @@ export function SEO({
       <link rel="canonical" href={canonicalUrl} />
 
       {/* Favicon */}
-      <link rel="icon" type="image/svg+xml" href="/EventGrid-favicon.svg" />
-      <link rel="shortcut icon" href="/EventGrid-favicon.svg" />
+      <link rel="icon" type="image/png" href="/favicon/favicon-96x96.png" sizes="96x96" />
+      <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
+      <link rel="shortcut icon" href="/favicon/favicon.ico" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+      <link rel="manifest" href="/favicon/site.webmanifest" />
 
       {/* Open Graph / Facebook */}
       <meta property="og:type" content={type} />
